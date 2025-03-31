aca063494e415f4fa198d13db39f21f68b3d0889
GanaviNagesh <143884911+GanaviNagesh@users.noreply.github.com>
2025-03-31 06:59:46 +0000

    data

---
+++ 01_basics/varaiables.js
@@ -0,0 +1,13 @@
+const accountId=145
+let accountEmail="abc@gmail.com"
+var accountPassword="123456789"
+accountCity='Banglore'
+console.table([accountId,accountEmail,accountPassword,accountCity])
+//accountId=2
+console.log(accountId);
+accountEmail="hc@gmail.com"
+accountPassword="12345"
+accountCity="Jaipur"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+let accountState;
+console.log(accountState);
