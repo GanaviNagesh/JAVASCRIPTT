5e6b65b2d199b60445ae1363919da03d75292aa6
GanaviNagesh <143884911+GanaviNagesh@users.noreply.github.com>
2025-03-31 08:00:11 +0000

    commit

---
+++ 01_basics/operations.js
@@ -0,0 +1,24 @@
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);//power
+console.log(2/2);
+console.log(2%2);
+
+
+let str1="ganavi"
+let str2=" nagesh"
+let str3=str1+str2
+console.log(str3);
+
+console.log(2+"6");
+console.log(2+2+"6");
+console.log("2"+2);
+console.log("2"+2+2);
+
+
+
